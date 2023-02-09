2b01e6896b18a3507e30ddd56ecb9c99640f57a6
likury <54955597+likury@users.noreply.github.com>
2023-02-09 00:42:21 -0300

    enunciado projeto de modelagem

---
+++ projeto_modelagem.sql
@@ -0,0 +1,21 @@
+/*Considere o banco de dados de um hospital.  De acordo com os requisitos a seguir, utilize o MER para representar o banco de dados desta livraria.
+1.  O hospital possui várias alas.
+2.  Cada ala possui uma enfermeira responsável.
+3.  Cada enfermeira reporta- se a uma enfermeira-chefe.
+4.  Enfermeiras podem atender apenas uma ala.
+5.  O hospital atende (credencia) os planos de saúde A, B e C.
+6.  Para cada plano de saúde,  é necessário saber os médicos credenciados no mesmo.
+7.  Médico tem CRM e enfermeira CRE que lhes sao  únicos.
+8.  Todo atendimento de um médico a um paciente deve ser registrado com a data e hora em que o
+O mesmo ocorreu.
+9.  Um mesmo paciente pode ser atendido por mais de um médico.
+10.  O hospital tem CNPJ.
+11.  A ala do hospital tem um identificador.
+12.  O plano de saúde tem um nome e telefone da operadora.
+13.  Médicos têm nome e especialidade.
+14.  Enfermeiras têm nome.
+15.  O nome de um plano de saúde é único*/
+
+Entrega:
+A entrega deve ser feita por meio de um link de um repositório público e 
+encaminhado para o meu email. O assunto do email deve ser [952 - MODELAGEM][NOME1-NOME2]
